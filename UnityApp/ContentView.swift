eaad0a392d8f5bb9e37a67a21cf1be66bb02d3b0
Greggory <greggory@greggorys-mbp.attlocal.net>
2020-06-02 22:16:28 -0400

    initial xcode project creation

---
+++ UnityApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UnityApp
+//
+//  Created by Greggory on 6/2/20.
+//  Copyright © 2020 Greggory. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
